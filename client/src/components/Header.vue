4ef608d1983bcdfe845803f0a2f700c224b6a1d1
Ahmed Ali <ahmed.alawadly@gmail.com>
2017-11-18 13:30:31 +0200

    added songs component

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,19 +1,21 @@
 <template>
     <v-toolbar fixed class="cyan" dark>
-        <router-link tag="v-toolbar-title" class="home mr-4" to="/">TabTracker</router-link>
-        <!--TODO-->
-        <!-- <v-toolbar-items>
-            <v-btn flat dark>
+        <router-link tag="v-toolbar-title" class="home mr-4" :to="{name: 'root'}">TabTracker</router-link>
+        <v-toolbar-items>
+            <v-btn to="songs" flat dark>
                 Browse
             </v-btn>
-        </v-toolbar-items> -->
+        </v-toolbar-items>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-            <v-btn v-if="!$store.state.isUserLoggedIn" to="login" flat dark>
+            <v-btn v-if="!$store.state.isUserLoggedIn" :to="{name: 'login'}" flat dark>
                 Login
             </v-btn>
-            <v-btn v-if="!$store.state.isUserLoggedIn" to="register" flat dark>
+            <v-btn v-if="!$store.state.isUserLoggedIn" :to="{name: 'register'}" flat dark>
                 Sign Up
+            </v-btn>
+            <v-btn v-if="$store.state.isUserLoggedIn" @click="logout" flat dark>
+                Logout
             </v-btn>
         </v-toolbar-items>
     </v-toolbar>
@@ -21,6 +23,15 @@
 
 <script>
 export default {
+    methods:{
+        logout () {
+            this.$store.dispatch('setToken', null)
+            this.$store.dispatch('setUser', null)
+            this.$router.push({
+                name: 'root'
+            })
+        }
+    }
 }
 </script>
 
---
+++ client/src/services/SongsService.js
@@ -0,0 +1,10 @@
+import Api from '@/services/Api.js'
+
+export default {
+    index() {
+        return Api().get('songs')
+    },
+    post(song) {
+        return Api().post('songs', song)
+    }
+}
